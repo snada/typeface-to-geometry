ca437732e1300cbe2870655c1cd838b218a448e2
snada <stefano.nada@gmail.com>
2017-01-13 13:32:46 +0100

    Forcing number in height reducer

--- reducers/height.js
+++ reducers/height.js
@@ -5,7 +5,7 @@ const DEFAULT_HEIGHT = 0;
 export default (state = DEFAULT_HEIGHT, action) => {
   switch (action.type) {
     case HEIGHT_CHANGED:
-      return action.height;
+      return +action.height;
 
     case BACK_PRESSED:
       return DEFAULT_HEIGHT;
