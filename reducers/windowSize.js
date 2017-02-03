622e72397c8b993cdeb17b8249736117963f118a
snada <stefano.nada@gmail.com>
2017-02-03 12:00:38 +0100

    Fixing windowSize reducer

--- reducers/windowSize.js
+++ reducers/windowSize.js
@@ -1,6 +1,6 @@
 import { WINDOW_RESIZED } from '../actions';
 
-export default (state = { x: 0, y: 0 }, action) => {
+export default (state = { width: 0, height: 0 }, action) => {
   switch (action.type) {
     case WINDOW_RESIZED:
       return { width: action.width, height: action.height };
